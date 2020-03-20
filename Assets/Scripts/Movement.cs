555233361e5cc91149fff7001d48e6db2858a441
Callum-Slowley <56228888+Callum-Slowley@users.noreply.github.com>
2020-03-20 22:23:06 +0000

    wall jump

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+    private Vector3 moveDirection;
+    CharacterController controller;
+    public bool wallCol;
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection.x = speed * Input.GetAxis("Horizontal");
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+    void Walljump()
+    {
+ 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+         if(hit.normal.y< 0.1&& !controller.isGrounded)
+        {
+            wallCol = true;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDirection = hit.normal * speed;
+                moveDirection.y = jumpSpeed;
+            }
+            else
+            {
+                wallCol = false;
+            }
+        }
+    }
+}
