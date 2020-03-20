694d4b186b127d85409e467204678f2965c42bb4
Callum-Slowley <56228888+Callum-Slowley@users.noreply.github.com>
2020-03-20 22:48:55 +0000

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,6 @@ public class Movement : MonoBehaviour
     public float speed;
     public float jumpSpeed;
     public float gravity;
-    public float sliding;
     private Vector3 moveDirection;
     CharacterController controller;
     public bool wallCol;
@@ -22,14 +21,7 @@ public class Movement : MonoBehaviour
                 moveDirection.y = jumpSpeed;
             }
         }
-        if (!wallCol)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
-        else if(wallCol)
-        {
-            moveDirection.y -= sliding * Time.deltaTime;
-        }
+        moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
     }
     // Start is called before the first frame update
@@ -56,10 +48,7 @@ public class Movement : MonoBehaviour
                 moveDirection = hit.normal * speed;
                 moveDirection.y = jumpSpeed;
             }
-            else
-            {
-                wallCol = false;
-            }
+
         }
     }
 }
