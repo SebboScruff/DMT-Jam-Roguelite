068d2b23182aaabae9874739439a58a353f4c5ce
Callum-Slowley <56228888+Callum-Slowley@users.noreply.github.com>
2020-03-20 19:45:06 +0000

    pushing basic moevement

---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public float jumpHight;
+    public float speed;
+    Rigidbody rb;
+    private float forwardsBackwards;
+    public bool isgrounded;
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpHight);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardsBackwards = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        rb.AddForce(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Ground")
+        {
+            isgrounded = true;
+        }
+        else
+        {
+            isgrounded = false;
+        }
+    }
+
+}
