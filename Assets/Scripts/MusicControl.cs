86342a0c2aa79aee3a0eaf6d3c47587f84d62dbd
ManInBlue917 <christianroyle99@gmail.com>
2020-03-21 00:07:56 +0000

    FMOD Intergration

---
+++ Assets/Scripts/MusicControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+   
+
+public class MusicControl : MonoBehaviour
+{
+    [Range(0f, 1f)]public float vapourMeter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<StudioEventEmitter>().EventInstance.setParameterByName("VapourMeter", vapourMeter);
+    }
+}
