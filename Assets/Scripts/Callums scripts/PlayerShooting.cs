33a6efb3b30dcfe19855a0f0736885227ead9edc
Callum-Slowley <56228888+Callum-Slowley@users.noreply.github.com>
2020-03-21 17:04:01 +0000

    crosshair

--- Assets/Scripts/Callums scripts/PlayerShooting.cs
+++ Assets/Scripts/Callums scripts/PlayerShooting.cs
@@ -7,9 +7,9 @@ public class PlayerShooting : MonoBehaviour
     public GameObject player;
     public GameObject bullet;
     public GameObject firingPoint;
-    public GameObject crosshair;
+    public Texture2D crosshairs;
     public float bulletSpeed;
-    private Vector3 target;
+    private Rect target;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,13 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,-10));
-        crosshair.transform.position = new Vector2(target.x, target.y);
+        //target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        //crosshairs.transform.position = new Vector2(target.x, target.y);
+
+    }
+    private void OnGUI()
+    {
+        target= new Rect((Input.mousePosition.x - (crosshairs.width / 2)), ((Screen.height - Input.mousePosition.y) - (crosshairs.height / 2)), crosshairs.width, crosshairs.height);
+        GUI.DrawTexture(target, crosshairs);
     }
 }
