cf81157893a6bf9747cb9c2204e0875ab4682d80
ManInBlue917 <christianroyle99@gmail.com>
2020-03-21 16:14:25 +0000

    Music No Kill

--- Assets/Scripts/MusicControl.cs
+++ Assets/Scripts/MusicControl.cs
@@ -7,7 +7,21 @@ using FMODUnity;
 
 public class MusicControl : MonoBehaviour
 {
+    //Use this value to change the vapour of the music
     [Range(0f, 1f)]public float vapourMeter;
+
+    private void Awake()
+    {
+        //Don't destroy the music on transitioning level
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +32,7 @@ public class MusicControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Checks and changes the vapour meter in FMOD
         GetComponent<StudioEventEmitter>().EventInstance.setParameterByName("VapourMeter", vapourMeter);
     }
 }
