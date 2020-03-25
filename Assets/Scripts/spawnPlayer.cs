ca35400085d30867150d076e49ec7544b5520d5a
JamesPhilipPrice <55580682+JamesPhilipPrice@users.noreply.github.com>
2020-03-25 20:19:37 +0000

    Got tythe player to spwn

---
+++ Assets/Scripts/spawnPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPlayer : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public levelgenerator levelGen;
+    public string levelGenName;
+
+    private void Start()
+    {
+        levelGen = GameObject.Find(levelGenName).GetComponent<levelgenerator>();
+    }
+
+    private void Update()
+    {
+        if (levelGen.stopGen == true)
+        {
+            Instantiate(playerPrefab, transform.position, Quaternion.identity);
+            Destroy(this);
+        }
+    }
+
+}
