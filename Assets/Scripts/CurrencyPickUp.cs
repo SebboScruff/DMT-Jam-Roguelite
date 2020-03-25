5daab0b825c1c94f5e7c909917172d8219a33ecb
SebboScruff <36053779+SebboScruff@users.noreply.github.com>
2020-03-25 13:01:46 +0000

    started work on a currency item

---
+++ Assets/Scripts/CurrencyPickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyPickUp : MonoBehaviour
+{
+    [Range(5, 75)]
+    public float turnSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0f, turnSpeed, 0f) * Time.deltaTime;
+
+
+    }
+}
