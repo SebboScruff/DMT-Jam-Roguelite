3decd94bf4c698e353a7dabfa039233285c159e6
sam prewitt <sam.prewitt@mail.bcu.ac.uk>
2020-03-25 15:46:53 +0000

    atrocities

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothingSpeed = 0.125f;
+    public Vector3 cameraOffset;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + cameraOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothingSpeed);
+        transform.position = smoothedPosition;
+    }
+}
