c54a8ca4c500caf46e0a04654a5d350f93dcd6e2
JamesPhilipPrice <55580682+JamesPhilipPrice@users.noreply.github.com>
2020-03-26 12:35:30 +0000

    Added score canvas

---
+++ Assets/James'sProcGenTestbed/scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int scoreValue;
+    public GameObject scoreCanvas;
+    public string playerName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.name == playerName)
+        {
+            Instantiate(scoreCanvas, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
