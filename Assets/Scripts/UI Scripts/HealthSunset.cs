b9a3184d74ca4cf0ed4b0d1ec973f1bd2105ced7
sam prewitt <sam.prewitt@mail.bcu.ac.uk>
2020-03-27 00:09:48 +0000

    Music Work for Sim

---
+++ Assets/Scripts/UI Scripts/HealthSunset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class HealthSunset : MonoBehaviour
+{
+    
+    public GameObject sceneSettings;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
