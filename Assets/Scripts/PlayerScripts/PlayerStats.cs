8ceb9ec22c76c8f6bee5575f7f777eda266060d1
Callum-Slowley <56228888+Callum-Slowley@users.noreply.github.com>
2020-03-27 13:17:24 +0000

    Score across scenes

--- Assets/Scripts/PlayerScripts/PlayerStats.cs
+++ Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -7,7 +7,7 @@ public class PlayerStats : MonoBehaviour
 {
     public float health;
     [SerializeField] private float maxHealth = 5f;
-    public int score;
+    static public int score;
 
     public Image healthBar;
 
@@ -39,7 +39,7 @@ public class PlayerStats : MonoBehaviour
     {
         //insntantiate a fancy particle system please
         //maybe add a small delay so we can see it as well
-
+        PlayerPrefs.SetInt("Score", score);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         //Debug.Log("I died not clickbait");
     }
